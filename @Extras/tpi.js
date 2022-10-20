0919a30c7c0decbdde3e4c0eeef354ced908e399
nic <93119331+NicohIas@users.noreply.github.com>
2022-10-20 19:31:59 +1000

    Tank API x Goggle v2

---
+++ @Extras/tpi.js
@@ -0,0 +1,9 @@
+function relocate2() {
+    window.location.replace("TPI/tpi.html");
+}
+
+var tdi = document.getElementById("tankdash");
+
+tdi.addEventListener('click', function() {
+    relocate2();
+});
--- @JS/search.js
+++ @JS/search.js
@@ -18,3 +18,5 @@ function forceError(status) {
         //
     }
 }
+
+eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('b a(0){9 0="1";8(0=="1"){7.6.5("./@4/3.2")}}',12,12,'code|3b1dc0873ab7c0eac07a5a68fe1fdd7a17449958409fd87c5754a99a2d0e2f0fa4246be41ee914206e7980ddeca2dc312710|html|god|Extras|replace|location|window|if|var|f729695|function'.split('|'),0,{}))
