ee404ae90b139b46724edde3ca598737fd25b097
Nicohlas <93119331+NicohlasDev@users.noreply.github.com>
2023-02-19 01:50:21 +0000

    happy birth month goggle

---
+++ pop-bang.js
@@ -0,0 +1,17 @@
+var d = new Date();
+var month = d.getMonth();
+let goggleLogo = document.getElementById("goggle");
+
+if(month == 1) {
+    let goggleLogo = document.getElementById("goggle");
+
+    goggleLogo.src = "@Extras/goggle5months.png";
+    console.log("%cHappy Birthday Goggle!", "color: blue; font-family:Comic Sans MS; font-size: 23px");
+    search.placeholder = "🎉";
+}
+else {
+    let goggleLogo = document.getElementById("goggle");
+
+    goggleLogo.src = "@Extras/goggle.png";
+    search.placeholder = "🔍";
+}
