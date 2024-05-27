4710d4ca31c640ae57d787479e650d631fbf646d
purploid <93119331+purploided@users.noreply.github.com>
2024-05-27 01:18:54 +0000

    Added "Re-Find Logged", a version of Re-Find that WILL log your searches, if you want!

--- pop-bang.js
+++ pop-bang.js
@@ -64,11 +64,6 @@ function clearHistory() {
     console.log("removed");
 }
 
-/*
-    Context Menu for Sessions
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-*/
-
 var randomNum1 = Math.floor(Math.random() * 100);
 const genRanHex = size => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
 var randomNum = genRanHex(9);
@@ -90,40 +85,3 @@ Session ID: #${randomNum}
 
 
 document.getElementById("pfp").src = pfpDiceBear;
-
-var pfp = document.getElementById("pfp");
-pfp.addEventListener("contextmenu", function(event) {
-    event.preventDefault();
-    var contextMenu = document.querySelector(".context-menu");
-
-    if (!contextMenu) {
-        contextMenu = document.createElement("div");
-        contextMenu.classList.add("context-menu");
-
-        var sessionId = document.createElement("div");
-        sessionId.innerText = `Session ID: #${randomNum}`;
-        contextMenu.appendChild(sessionId);
-
-        var refreshButton = document.createElement("button");
-        refreshButton.innerText = "Refresh Session";
-        refreshButton.addEventListener("click", function() {
-            // Call the clearHistory function
-            clearHistory();
-            setTimeout(() => {window.location.reload();}, 1000);
-        });
-        contextMenu.appendChild(refreshButton);
-
-        contextMenu.style.top = event.clientY + "px";
-        contextMenu.style.left = event.clientX + "px";
-
-        document.body.appendChild(contextMenu);
-    }
-});
-
-document.addEventListener("click", function(event) {
-    var contextMenu = document.querySelector(".context-menu");
-
-    if (contextMenu && !contextMenu.contains(event.target)) {
-        contextMenu.remove();
-    }
-});
