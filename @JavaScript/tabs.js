900ad66abc4f2dea30329a69758932e84bdf0e94
purploid <93119331+purploided@users.noreply.github.com>
2024-06-23 01:37:58 +0000

    Updated the tabs system.

--- @JavaScript/tabs.js
+++ @JavaScript/tabs.js
@@ -1,15 +1,21 @@
 let tabAdder = document.getElementById('addtab');
+let tab1btn = document.getElementById('tab1');
+let tab2btn = document.getElementById('tab2');
 let refindTabOne = document.getElementById('goggleOne');
 let refindTabTwo = document.getElementById('goggleTwo');
 
 tabAdder.addEventListener('click', function() {
-    document.addEventListener('keydown', function(event) {
-        if (event.key === '1') {
-            refindTabOne.style.display = 'block';
-            refindTabTwo.style.display = 'none';
-        } else if (event.key === '2') {
-            refindTabOne.style.display = 'none';
-            refindTabTwo.style.display = 'block';
-        }
-    });
+    tab2btn.style.display = 'block';
 });
+
+function tab1() {
+    refindTabOne.style.display = 'block';
+    refindTabTwo.style.display = 'none';
+}
+function tab2() {
+    refindTabOne.style.display = 'none';
+    refindTabTwo.style.display = 'block';
+}
+
+tab2btn.addEventListener('click', tab2);
+tab1btn.addEventListener('click', tab1);
